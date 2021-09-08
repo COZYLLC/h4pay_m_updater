1b070ffe2284f88e4c7cdc1f99ba41af030c85ee
KimWash <ckm0728wash@gmail.com>
2021-09-08 10:39:11 +0900

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="title">H4Pay 관리 앱 배포</div>
+    <div class="container">
+      <b-field label="헌재 버전" horizontal> {{ currVersion }} </b-field>
+      <b-field label="업데이트 대상 버전" horizontal>
+        {{ nextVersion }}
+      </b-field>
+      <b-field label="변경사항" horizontal>
+        <b-input type="textarea" v-model="changes" />
+      </b-field>
+      <b-field label="APK 파일 업로드" horizontal>
+        <b-upload v-model="dropFile" drag-drop accept=".apk">
+          <section class="section">
+            <div class="content has-text-centered">
+              <p>
+                <b-icon icon="upload" size="is-large"> </b-icon>
+              </p>
+              <p v-if="typeof(dropFile) == 'function'">파일을 선택하거나 드래그하세요</p>
+              <p v-if="(typeof(dropFile) != 'function')">{{dropFile.name}}</p>
+            </div>
+          </section>
+        </b-upload>
+      </b-field>
+      <b-field>
+        <b-button rounded style="float: right" @click="deploy">배포</b-button>
+      </b-field>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      dropFile: File,
+      currVersion: null,
+      nextVersion: null,
+      changes: ''
+    };
+  },
+  created() {
+    console.log(process.env);
+    this.$axios
+      .get(`${process.env.VUE_APP_API_URL}/version`)
+      .then((res) => {
+        this.currVersion = res.data.version;
+        this.nextVersion = (this.currVersion + 0.001).toFixed(3)
+      })
+      .catch((err) => {
+        alert(`현재 버전을 가져올 수 없습니다: ${err.message}`);
+      });
+  },
+  methods: {
+    deploy() {
+      console.log(`${this.currVersion} | ${this.nextVersion} | ${this.changes}`)
+      console.log(this.dropFile)
+      let form = new FormData();
+      form.append('currVersion', this.currVersion);
+      form.append('nextVersion', this.nextVersion);
+      form.append('changes', this.changes)
+      form.append('file', this.dropFile);
+
+      this.$axios.post(`${process.env.VUE_APP_API_URL}/version/update`, form).then(res => {
+        console.log(res)
+      })
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -23,6 +77,21 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+  width: 100vw;
+  padding: 10vw;
+}
+.container {
+  border: 1px solid #ccc !important;
+  border-radius: 15px;
+  padding: 20px;
+  padding-bottom: 6vh;
+}
+
+.upload {
+  width: 100% !important;
+}
+.upload-draggable {
+  width: 100% !important;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUpload } from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios';
+
+// init Fontawesome
+library.add(faUpload);
+Vue.component('vue-fontawesome', FontAwesomeIcon);
+
+// init buefy
+Vue.use(Buefy, {
+  defaultIconComponent: 'vue-fontawesome',
+  defaultIconPack: 'fas'
+});
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios
 
 new Vue({
   render: h => h(App),
